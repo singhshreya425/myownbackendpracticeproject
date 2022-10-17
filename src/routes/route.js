1c0922879fe705508bf695fa6d9e4ab4ee31ba4a
singhshreya425 <singhshreya425@gmail.com>
2022-10-17 16:34:59 +0530

    new commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,32 @@
 const express = require('express');
-const router = express.Router();
+const abc = request('../introduction/intro')
+const myrouter = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My Batch is ',abc.name)
+    abc.printName()
+    res.send('abc')
+    res.send('My second ever api!')
 });
+router.get('/route', function (req,res){
+    console.log("Route handler")
+    res.send('hi')
+})
+router.get('/profile-details',function(req, res){
+    console.log('The path params in the request are : ',req.query)
+    let students =["sabiha","Akash","pritesh"]
+    res.send(students)
+})
+
+router.get('/students/sabiha',function(req,res){
+    res.send('The full name is sabiha')
+})
+router.get('/students/shreya',function(req,res){
+    res.send('The full name is pritesh')
+})
+router.get('/students/shreya',function(req,res){
+    res.send('The full name is Akash')
+})
+router.get('/students/:studentName')
 
 module.exports = router;
