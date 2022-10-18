6d79fa888edb761334eec929b1c21f4ee0fe8f3b
singhshreya425 <singhshreya425@gmail.com>
2022-10-18 21:29:51 +0530

    shreya singh

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,16 @@ router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
+router.get("/shoes",function(req, res){
+    let queryParams = req.query
+    let brand = queryParams.brand
+    let discount = queryParams.discount
+    let color = queryParams.color
+    console.log('The brand selected is ',brand)
+    console.log('The discount option selected ',discount)
+    console.log('The color selected is ', color)
+    res.send("dummy response")
+})
 
 
 // Example 1 for path params
