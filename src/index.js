4718759d3499a0aeaf018fa25aeb695cead22845
singhshreya425 <singhshreya425@gmail.com>
2022-10-21 00:35:38 +0530

    newcommit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 
-const route = require('./routes/route.js');
+const route = require('./routes/test.js');
+const route1 = require('./routes/route.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/testapi", function(req,res){
+    console.log("test")
+    res.send({data:"test api"})
+})
+
 app.use('/', route);
+app.use('/', route1);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -157,41 +157,41 @@ router.get('/films/:filmid', function (req, res) {
 // : 4 is missing
 // // Your route code will look like this
 // app.get("/sol1", function (req, res) {
-    // //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of
-    // numbers till last digit in the array
-    app.get("/sol1", function (req, res) {
-    let arr = [1, 2, 3, 5, 6, 7]
-    let total = 0;
-    for (var i in arr) {
-        total += arr[i];//total=total+arr[i]
-    }
-    let lastDigit = arr.pop()
-    let consecutiveSum = lastDigit * (lastDigit + 1) / 2
-    let missingNumber = consecutiveSum - total
-    res.send({ data: missingNumber });
-});
+// //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of
+// numbers till last digit in the array
+// router.get("/sol", function (req, res) {
+//     let arr = [1, 2, 3, 5, 6, 7]
+//     let total = 0;
+//     for (var i in arr) {
+//         total += arr[i];//total=total+arr[i]
+//     }
+//     let lastDigit = arr.pop()
+//     let consecutiveSum = lastDigit * (lastDigit + 1) / 2
+//     let missingNumber = consecutiveSum - total
+//     res.send({ data: missingNumber });
+// });
 
 // Q2.
 // // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33,
 // 34, 35, 37, 38]: 36 is missing
 // // Your route code will look like this
-app.get("/sol2", function (req, res) {
-    // //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all
-    // numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
-    let arr = [33, 34, 35, 37, 38]
-    let len = arr.length
+// router.get("/sol2", function (req, res) {
+//     // //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all
+//     // numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+//     let arr = [33, 34, 35, 37, 38]
+//     let len = arr.length
 
-    let total = 0;
-    for (var i in arr) {
-        total += arr[i];
-    }
-    let firstDigit = arr[0]
-    let lastDigit = arr.pop()
-    let consecutiveSum = (len + 1) * (firstDigit + lastDigit) / 2
-    let missingNumber = consecutiveSum - total
+//     let total = 0;
+//     for (var i in arr) {
+//         total += arr[i];
+//     }
+//     let firstDigit = arr[0]
+//     let lastDigit = arr.pop()
+//     let consecutiveSum = (len + 1) * (firstDigit + lastDigit) / 2
+//     let missingNumber = consecutiveSum - total
 
-    res.send({ data: missingNumber });
-});
+//     res.send({ data: missingNumber });
+// });
 // Assignments 2 (Structure of the routes file would look like what is given at the bottom of the document):
 // You have to write a POST apis:
 // Write the api in first project directory (Routes/index.js or routes/route.js)
@@ -209,37 +209,58 @@ app.get("/sol2", function (req, res) {
 // }
 // e.g. the players array would look like this:
 // let players =
-//    [
-//        {
-//            "name": "manish",
-//            "dob": "1/1/1995",
-//            "gender": "male",
-//            "city": "jalandhar",
-//            "sports": [
-//                "swimming"
-//            ]
-//        },
-//        {
-//            "name": "gopal",
-//            "dob": "1/09/1995",
-//            "gender": "male",
-//            "city": "delhi",
-//            "sports": [
-//                "soccer"
-//            ]
-//        },
-//        {
-//            "name": "lokesh",
-//            "dob": "1/1/1990",
-//            "gender": "male",
-//            "city": "mumbai",
-//            "sports": [
-//                "soccer"
-//            ]
-//        },
-//    ]
+//     [
+//         {
+//             "name": "manish",
+//             "dob": "1/1/1995",
+//             "gender": "male",
+//             "city": "jalandhar",
+//             "sports": [
+//                 "swimming"
+//             ]
+//         },
+//         {
+//             "name": "gopal",
+//             "dob": "1/09/1995",
+//             "gender": "male",
+//             "city": "delhi",
+//             "sports": [
+//                 "soccer"
+//             ]
+//         },
+//         {
+//             "name": "lokesh",
+//             "dob": "1/1/1990",
+//             "gender": "male",
+//             "city": "mumbai",
+//             "sports": [
+//                 "soccer"
+//             ]
+//         },
+//     ]
+// router.post('/players', function (req, res) {
+//     const body = req.body
+    // {
+    //                "name": "lokesh",
+    //                "dob": "1/1/1990",
+    //                "gender": "male",
+    //                "city": "mumbai",
+    //                "sports": [
+    //                    "soccer"
+    //                ]
+    //            }
+
+//     const playerDetail = player.find(player => player.name === body.name)
+//     if (player) {
+//         res.send({ message: "player already exist" })
+//     } else {
+//         players.push(body)
+//         res.send(players)
+//     }
+// })
 
 
+// ASSIGNMENT 2
 // Write a POST /players api that creates a new player ( i.e. that saves a player’s details and doesn’t allow saving the data of a player with a name that already exists in the data)
 
 // NOTE: you must create the players array outside( on the top ) of the api( so that data is maintained across api hits)
@@ -252,55 +273,181 @@ app.get("/sol2", function (req, res) {
 // The file inside routes would look like this:
 
 // const express = require('express');
-// const router = express.Router();
+// constrouter = express.Router();
 
-// let players =
-//    [
-//        {
-//            "name": "manish",
-//            "dob": "1/1/1995",
-//            "gender": "male",
-//            "city": "jalandhar",
-//            "sports": [
-//                "swimming"
-//            ]
-//        },
-//        {
-//            "name": "gopal",
-//            "dob": "1/09/1995",
-//            "gender": "male",
-//            "city": "delhi",
-//            "sports": [
-//                "soccer"
-//            ],
-//        },
-//        {
-//            "name": "lokesh",
-//            "dob": "1/1/1990",
-//            "gender": "male",
-//            "city": "mumbai",
-//            "sports": [
-//                "soccer"
-//            ],
-//        },
-//    ]
+let player =
+    [
+        {
+            "name": "manish",//string
+            "dob": "1/1/1995",//string
+            "gender": "male",//string
+            "city": "jalandhar",//string
+            "sports": [
+                "swimming"   //array can be more then one
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
 
-router.post('/players1', function (req, res) {
-    console.log(players)
-    let newplayer = req.body.p1
-    console.log(newplayer)
-    for (i of players) {
-        if (i.name == newplayer.name) {
-            return res.send("name already exists")
+// router.post('/player1', function (req, res) {
+//     console.log(player)
+//     let newplayer = req.body.p1
+//     console.log(newplayer)
+//     for (i of player) {
+//         if (i.name == newplayer.name) {
+//             return res.send("name already exists")
+//         }
+//     }
+
+//     player.push(newplayer)
+//     console.log(result)
+//     res.send({ msg: player, status: true })
+// });
+
+router.post('/hello', function(req,res){
+    res.send({msg : "hello"})
+})
+
+let players = []
+
+router.post('/newplayers', function (req, res) {
+    
+    console.log("hello")
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    let player = player.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < player.length; i++) {
+        if(player[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
         }
     }
 
-    players.push(newplayer)
-    //console.log(result)
-    res.send({ msg: players, status: true })
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        player.push(newPlayer)
+        res.send(player)
+    };
+});
+
 
     //LOGIC WILL COME HERE
-})
+    //path params
+    // router.get("/wiki/:countryName", function (req, res) {
+    //     res.send({ data: "something data" })
+    // })
+    // router.get("/:product")
+    // //Query params 
+    // //use case: to make filters/ searches
+    // //variable name is visible in the url itself
+    // router.get("/get-query-1", function (req, res) {
+    //     let data = req.query
+    //     let var1 = data.myCoolVar
+    //     let var2 = data.xyz
+    //     console.log(data)
+    //     res.send({ data: data, status: true })
+    // })
+    // //take marks in req.query in a variable named "marks"
+    // router.get("get-query-2", function (req, res) {
+    //     let marks = req.query.marks
+    //     let result = marks > 40 ? "pass" : "fail"
+    //     //ternary operator
+    //     res.send({ data: result, status: true })
+    // })
+    //query params (as well  as path params) can be sent
+    // router.post("/post-query-1", function (req, res) {
+    //     let data = req.query
+    //     console.log(data)
+    //     res.send({ data: data, status: true })
+    // })
+    // //filter out all the numbers that are greater than ""
+    let myArr = [23, 45, 67, 281394, 423, 24, 42323]
+    router.post("/post-query-2", function (req, res) {
+        let input = req.query.input
+        // let finArr= myArr.filter(ele => ele>input)
+        let finalArr = []
+        for (i = 0; i < myArr.length; i++) {
+            if (myArr[i] > input) finalArr.push(myArr[i])
+
+        }
+        res.send({ data: finalArr, status: true })
+    })
+    //     ASSIGNMENT:
+
+    // ASSIGNMENT:
+    // you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+    // take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+    // also return an array consisting of only the person that can vote
+
+    // WRITE A POST API TO THE ABOVE
+
+
+    // take this as sample for array of persons:
+//     let persons = [
+//         {
+//             name: "PK",
+//             age: 10,
+//             votingStatus: false
+//         },
+//         {
+//             name: "SK",
+//             age: 20,
+//             votingStatus: false
+//         },
+//         {
+//             name: "AA",
+//             age: 70,
+//             votingStatus: false
+//         },
+//         {
+//             name: "SC",
+//             age: 5,
+//             votingStatus: false
+//         },
+//         {
+//             name: "HO",
+//             age: 40,
+//             votingStatus: false
+//         }
+//     ]
+    
+//     router.post("/name", function(req, res){
+//         console.log("hi")
+//         let newVotingAge=[]
+//         let votingAge = req.query.votingAge
+//         for(let i=0; i<persons.length; i++){
+//             if(persons[i].age>votingAge){
+//                 persons[i].votingStatus=true;
+//                 newVotingAge.push(persons[i])
+//             }
+//         }
+//             res.send({data:newVotingAge, status:true})
+
+// })
 
 
 
