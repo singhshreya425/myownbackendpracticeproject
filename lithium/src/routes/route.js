4af789100647c06ea1bf0e3af2f2bca6b3cd1175
ravisingh007ravi <ravisingh007ravi.com>
2022-11-14 13:53:35 +0530

    add

---
+++ lithium/src/routes/route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/test-me",function(req,res){
+    res.send("hiii")
+})
+
+router.get("/");
+
+module.exports=router;
