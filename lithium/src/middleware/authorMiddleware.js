b42d480188178961ee2e313c6fa6d120ebcb041d
singhshreya425 <singhshreya425@gmail.com>
2022-11-20 18:42:31 +0530

    doneoverall

--- lithium/src/middleware/authorMiddleware.js
+++ lithium/src/middleware/authorMiddleware.js
@@ -6,13 +6,13 @@ const authenticate = (req, res, next) => {
 
         if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
 
-        jwt.verify(token, "blogging site", function (err, decode) {
+        jwt.verify(token, "Secret-Key-lithium", function (err, decode) {
           if (err) { return res.status(401).send({ status: false, data: "Authentication failed" }) }
           req.decode = decode;
           next();
       })
 
-        if (!decode) return res.status(401).send({ status: false, msg: "token is invalid" });
+       
       
         
       
