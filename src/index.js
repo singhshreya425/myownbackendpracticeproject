8bbef5040ea4bfb29222439e37054463cd41bfb8
Priyanka9120 <priyanka912066@gmail.com>
2022-12-19 15:04:16 +0530

    first task done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const route = require("./routes/route")
+const app = express()
+
+const mongoose = require("mongoose")
+const multer=require("multer")
+app.use(express.json())
+app.use(multer().any())
+
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect("mongodb+srv://priyanka912066:Pie5MEDyx8B1zOiq@cluster0.ucnslwp.mongodb.net/Group18Database",
+    { useNewUrlParser: true })
+    .then(() => console.log(("Mongoose is connected")))
+    .catch(err => console.log(err.message))
+
+app.use("/", route)
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express is running on port " + (process.env.PORT || 3000))
+})
+
+
+
+
